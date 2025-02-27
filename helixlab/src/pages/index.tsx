c07795d8125e930e2fea7b1b57c3b42e330ea8c7
weimao <luanty@163.com>
2025-02-28 02:11:13 +0800

    new main page

--- helixlab/src/pages/index.tsx
+++ helixlab/src/pages/index.tsx
@@ -4,6 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import GridDistortion from '@site/src/components/GridDistortion/GridDistortion';
 
 import styles from './index.module.css';
 import Translate from '@docusaurus/Translate';
@@ -37,9 +38,36 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+        
+      {/* <HomepageHeader /> */}
       <main>
-        <HomepageFeatures />
+        {/* <HomepageFeatures /> */}
+        <div style={{ width: '100%', height: '75vh', position: 'relative' }}>
+        <GridDistortion
+          imageSrc="https://cdn.shopify.com/s/files/1/0444/8259/2928/files/index.jpg"
+          grid={10}
+          mouse={0.1}
+          strength={0.15}
+          relaxation={0.9}
+          className="custom-class"
+        />
+        <div
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-125%, -230%)',
+              textAlign: 'center',
+              fontSize: '5rem',
+              fontWeight: 900,
+              userSelect: 'none',
+              mixBlendMode: 'difference',
+              pointerEvents: 'none',
+              color: 'difference',
+            }}>
+            Helix Lab Docs.
+          </div>
+      </div>
       </main>
     </Layout>
   );
